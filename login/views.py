2888a507a68e0a39b985fcd0b1dfdfd2739357cb
melnychukk <melnychukk18@gmail.com>
2015-03-24 09:48:18 +0100

    Html templates. Login page view. Login form. Registration form.

--- login/views.py
+++ login/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from forms import LoginForm
 
-# Create your views here.
+def loginpage(request):
+	loginform = LoginForm()
+	return render(request, 'login/login-index.html',{
+			'loginform':loginform
+		})
+
+@require_http_methods(["POST"])
+def login(request):
+	pass
+
+
+@require_http_methods(["POST"])
+def register():
+	pass
--- todobox/settings.py
+++ todobox/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login',
+    'tasks'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -80,4 +82,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL =  '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+TEMPLATE_DIRS = (
+    BASE_DIR + '/templates/',
+)
